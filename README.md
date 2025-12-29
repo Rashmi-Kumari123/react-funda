a5812ab4099e728a041548c2442170ea67fb8308
Rashmi-kumari123 <rashu2377@gmail.com>
2025-12-29 17:56:29 +0530

    used formik library while managing form

--- README.md
+++ README.md
@@ -5,42 +5,55 @@
 React is a framework that allows me to build complex web applications like HotStar.
 
 ### Components
+
 React apps are made out of components. A component is a piece of the UI (user interface) that has its own logic and appearance. A component can be as small as a button, or as large as an entire page.
 
 ### JSX Files
+
 `.jsx` files give me the facility to write HTML + JavaScript together.
+
 - Inside curly braces, write your variable name.
 - Using props, we pass value from parent to child using variable as props.
 
 ### State Management
+
 **How we manage state and handle click:**
+
 - I want as book ticket button clicked by someone, increase total booking count dynamically
 - We use `useState` hook to manage state
 - `useState` holds the data temporarily but not permanently
 
 ### Rendering Lists
+
 How we can render list of movies from backend to UI using `map` function.
 
 ### Routing
+
 Learned to create multiple pages and how to navigate one page from another page using `react-router-dom`.
+
 - Like if I am hovering on login, it should redirect to login page
 
-### How to handle form in react
-Learned how to use handleChange and handleSubmit while filling login form. one option is create separate state for each field but other option is just use single state.
+### Form Handling in React
 
+**Basic Form Handling:**
 
-### Application Entry Point
-Inside `index.js` we are rendering whole app.
+Learned how to use `handleChange` and `handleSubmit` while filling login form. One option is to create separate state for each field, but another option is to just use a single state.
 
-### Router Setup
-If we want route should available for our whole application, we need to wrap App component inside `BrowserRouter` and this `BrowserRouter` is coming from `react-router-dom`.
+**Using Formik Library:**
 
-### How to handle form using formik library
 Formik is a small group of React components and hooks for building forms in React. It helps with the three most annoying parts:
 
--Getting values in and out of form state
--Validation and error messages
--Handling form submission
+- Getting values in and out of form state
+- Validation and error messages
+- Handling form submission without you having to write `onChange` and `onSubmit` logic - Formik will do it easily
+
+### Application Entry Point
+
+Inside `index.js` we are rendering the whole app.
+
+### Router Setup
+
+If we want routes to be available for our whole application, we need to wrap the App component inside `BrowserRouter` and this `BrowserRouter` is coming from `react-router-dom`.
 
 ---
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,30 +1,39 @@
 import { React, useState } from 'react'
+import { useFormik } from 'formik'
 
 const Login = () => {
-  const [formData, setFormData] = useState({
-    fullName : "",
-    email : "",
-    password : ""
+  // const [formData, setFormData] = useState({
+  //   fullName : "",
+  //   email : "",
+  //   password : ""
 
-  })
-  const handleChange=(e)=>{
-    const {value, name} = e.target;
-    setFormData({...formData, [name]: value});
-
-    
-  }
-  const handleSubmit=(e)=>{
-    e.preventDefault(); // form will reload automatically
-    console.log("on submit", formData)
-  }
+  // })
+  // const handleChange=(e)=>{
+  //   const {value, name} = e.target;
+  //   setFormData({...formData, [name]: value});  
+  // }
+  // const handleSubmit=(e)=>{
+  //   e.preventDefault(); // form will reload automatically
+  //   console.log("on submit", formData)
+  // }
+  const formik = useFormik({
+    initialValues: {
+      fullName: "",
+      email: "",
+      password: ""
+    },
+    onSubmit: values => {
+      console.log("on submit", values);
+    },
+  });
   return (
     <div>
       <h1 className='font-bold text-2xl pb-6'>Login Form</h1>
-      <form onSubmit={handleSubmit} className='space-y-4'>
-        <div><input className='border ps-4 py-4 rounded-md lg:w-[33%] w-full' type='text' placeholder='Enter your fullName' name='fullName' onChange={handleChange} value = {formData.fullName}/></div>
-      <div><input className='border ps-4 py-4 rounded-md lg:w-[33%] w-full' type='text' placeholder='Enter your email' name='email' onChange={handleChange}  value = {formData.email}/></div>
-      <div><input className='border ps-4 py-4 rounded-md lg:w-[33%] w-full' type='text' placeholder='Enter your password' name='password'onChange={handleChange} value = {formData.password}/></div>
-      <button type="submit" className='lg:w-[33%] bg-slate-300 py-3 rounded-md'>Login</button>
+      <form onSubmit={formik.handleSubmit} className='space-y-4'>
+        <div><input className='border ps-4 py-4 rounded-md lg:w-[33%] w-full' type='text' placeholder='Enter your fullName' name='fullName' onChange={formik.handleChange} value={formik.values.fullName} /></div>
+        <div><input className='border ps-4 py-4 rounded-md lg:w-[33%] w-full' type='text' placeholder='Enter your email' name='email' onChange={formik.handleChange} value={formik.values.email} /></div>
+        <div><input className='border ps-4 py-4 rounded-md lg:w-[33%] w-full' type='password' placeholder='Enter your password' name='password' onChange={formik.handleChange} value={formik.values.password} /></div>
+        <button type="submit" className='lg:w-[33%] bg-slate-300 py-3 rounded-md'>Login</button>
       </form>
     </div>
   )
